5d48b261fef4d2e4f2f68289d4b55a7622eec983
DoYeon Ahn <duseh729@naver.com>
2023-01-05 13:13:04 +0900

    버튼 조건 활성화 구현

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [btnStatus, setBtnStatus] = useState("");
   const createUser = async () => {
     if (isEmail(email)) {
       if (password.length >= 8) {
         const { res } = await axios.post("http://localhost:3001/user", { email, password });
+        alert("회원가입이 완료됨.");
+        window.location.href = "/";
       } else {
         alert("비밀번호를 8자 이상으로 입력해주세요.");
       }
     } else {
       alert("이메일에 @와 .을 포함해주세요.");
     }
-
+    setBtnStatus("");
     setEmail("");
     setPassword("");
   };
@@ -25,19 +29,50 @@ export default function SignUp() {
         <div>
           <label>이메일</label>
           <br />
-          <input value={email} required type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
+          <input
+            value={email}
+            required
+            type="email"
+            name="email"
+            onChange={(e) => {
+              if (e.target.value.includes("@") && e.target.value.includes(".")) {
+                setBtnStatus("1");
+                if (e.target.parentNode.parentNode.childNodes[1].childNodes[2].value.length > 7) {
+                  console.log("hi");
+                  e.target.parentNode.parentNode.childNodes[2].className = "btn-design";
+                }
+              } else {
+                setBtnStatus("");
+                e.target.parentNode.parentNode.childNodes[2].className = "btn-design disabled";
+              }
+              setEmail(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label>비밀번호</label>
           <br />
-          <input value={password} required type="password" name="pwd" onChange={(e) => setPassword(e.target.value)} />
+          <input
+            value={password}
+            required
+            type="password"
+            name="pwd"
+            onChange={(e) => {
+              if (e.target.value.length > 7 && btnStatus == "1") {
+                e.target.parentNode.parentNode.childNodes[2].className = "btn-design";
+              } else {
+                e.target.parentNode.parentNode.childNodes[2].className = "btn-design disabled";
+              }
+              setPassword(e.target.value);
+            }}
+          />
         </div>
         <button
           onClick={(e) => {
             e.preventDefault();
             createUser();
           }}
-          className="btn-design"
+          className="btn-design disabled"
         >
           회원가입
         </button>
